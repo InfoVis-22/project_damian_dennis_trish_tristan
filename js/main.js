5ec71df81d866722b765cf5b45c089568698cbd2
Trish Beekeeper <chickabuzz@gmail.com>
2022-04-30 09:02:39 -0400

    javascript pulls data from files, have 2 arrays ready to be graphed

--- js/main.js
+++ js/main.js
@@ -1,43 +1,53 @@
-d3.csv('data/deathsByCounty/Ohio_Resident_Deaths_2007_cty.csv')
-    .then(data2007 => {
-      dataInFcn(data2007);
-    })
-    .catch(error => {
-        console.log("error loading the data right???");
-    })
+//Get data in from file. 
+//Note that must do all script inside .then(), because it is a "promise"
+//that data from file is incoming.
+//Data (initially all string type):
+//DeathCOD39ICDCode39Desc: cause of death, 39 possible
+//DeathCountyCountyName: county of death
+//Deaths:
+//Population:
 
-function dataInFcn(data) {
+Promise.all([
+  d3.csv('data/deathsByCounty/Ohio_Resident_Deaths_2007_cty.csv'),
+  d3.csv('data/demographics/countyHealthRatings2010.csv')])
+  .then(([data1,data2]) => {
+    dataInFcn(data1,data2);
+  })
+
+function dataInFcn(data,demog) {
+  //transform string type into numerical type
+  //and we are serious programmers, we don't need SORT
+  data.forEach(function (d) { 
+    d.Deaths = +d.Deaths;
+    d.Population = +d.Population;
+    delete d.SORT;
+  });
+
+  //remove non-ohio counties of death???
+  data = data.filter(d=> d.DeathCountyCountyName !="Unknown" && d.DeathCountyCountyName !="NonOH");
+
+  //get cause of death of interest
   var data2007 = data.filter(function(d){
     return d.DeathCOD39ICDCode39Desc=="Influenza and pneumonia (J09-J18)";
   });
+  //console.log(data2007);
+
+  //get total number of deaths
+  let totalDeaths = data.map(d=>d.Deaths).reduce((a,c)=>a+c);
+
+  //get counties, going to use this for graphing
+  let countiesGraphArray = data2007.map(d=>d.DeathCountyCountyName);
+  //console.log(countiesGraphArray);
   
-  //data2007.Deaths = parseFloat(data2007.Deaths);
-  //data2007.Population = parseFloat(data2007.Population);  
-  
-  data2007.forEach(function (d) { 
-    d.Deaths = +d.Deaths;
-    d.Population = +d.Population;
-    d.pctDeaths = d.Deaths/d.Population * 100;
-    delete d.SORT;
+  //get deaths per 100,000 people, in each county, to account for population
+  let deathsRespiratoryGraphArray = data2007.map(d=>Math.round(d.Deaths/d.Population *100000));
+  console.log(deathsRespiratoryGraphArray);
 
-    //data2007.sort = data2007.Deaths/data2007.Population *100;
-    //data2007.add(pctDeaths: data2007.Deaths/data2007.Population *100);
-  });
-
-
- /*
-  data2007.defineProperty(data2007,'pctDeaths', {
-   value: data2007.Deaths/data2007.Population
- });
- */
- 
-
-  console.log(data2007);
-
-  drawData(data2007);
+  //function to actually make the graphs
+  drawData(countiesGraphArray, deathsRespiratoryGraphArray);
 }
 
-function drawData(data){
+function drawData(counties, deaths){
 
 }
 
